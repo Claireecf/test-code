19f055baa496bcf9811ee4a00a4b62393f641450
Claireecf <89868984+Claireecf@users.noreply.github.com>
2022-11-04 13:51:32 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use std::env;
 use http_client::isahc::IsahcClient;
 use isahc::{HttpClient, prelude::Configurable, config::{VersionNegotiation, SslOption}};
-use opentelemetry::{
-    sdk::{
-        trace::{self},
-        Resource,
-    },
-    KeyValue,
-};
+use opentelemetry::{sdk::{trace::{self, Tracer}, Resource,}, KeyValue};
 
 mod shared;
 
@@ -21,27 +15,33 @@ async fn main() -> std::result::Result<(), http_types::Error>{
 
     femme::with_level(femme::LevelFilter::Info);
     shared::init_global_propagator();
-    let _tracer = opentelemetry_jaeger::new_agent_pipeline()
-        .with_service_name(SVC_NAME)
-        .with_trace_config(trace::config().with_resource(Resource::new(tags())))
-        .install_batch(opentelemetry::runtime::AsyncStd);
+    let _tracer = pipeline();
     let otl_mw = opentelemetry_surf::OpenTelemetryTracingMiddleware::default();
-    
-    //VersionNegotiation::http3(); Connect via HTTP/3
-    let http_client = HttpClient::builder()
-        .version_negotiation(VersionNegotiation::http2())
-        .ssl_options(SslOption::DANGER_ACCEPT_INVALID_CERTS | SslOption::DANGER_ACCEPT_REVOKED_CERTS | SslOption::DANGER_ACCEPT_INVALID_HOSTS)
-        .metrics(true)
-        .build().unwrap();
-    let isahc_client = IsahcClient::from_client(http_client);
-    let client = surf::Client::with_http_client(isahc_client).with(otl_mw);
-
+    let client = create_client().with(otl_mw);
 
     let response = client.get(url).recv_string().await?;
     println!("{}", response);
 
     opentelemetry::global::shutdown_tracer_provider();
     Ok(())
+}
+
+fn create_client() -> surf::Client {
+    let http_client = HttpClient::builder()
+    .version_negotiation(VersionNegotiation::http2())
+    .ssl_options(SslOption::DANGER_ACCEPT_INVALID_CERTS | SslOption::DANGER_ACCEPT_REVOKED_CERTS | SslOption::DANGER_ACCEPT_INVALID_HOSTS)
+    .metrics(true)
+    .build().unwrap();
+    let isahc_client = IsahcClient::from_client(http_client);
+    surf::Client::with_http_client(isahc_client)
+}
+
+fn pipeline() -> Tracer {
+    opentelemetry_jaeger::new_agent_pipeline()
+        .with_service_name(SVC_NAME)
+        .with_trace_config(trace::config().with_resource(Resource::new(tags())))
+        .install_batch(opentelemetry::runtime::AsyncStd)
+        .expect("pipeline install failure")
 }
 
 fn tags() -> Vec<KeyValue> {
